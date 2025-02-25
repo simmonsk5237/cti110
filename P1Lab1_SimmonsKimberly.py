929ba7ae3ffec0e74cbf004a222f1b671cc0d2ad
simmonsk5237 <simmonsk5237@student.faytechcc.edu>
2025-02-24 22:19:16 -0500

    Add files via upload
    P1Lab1 Assignment(First Assignment)

---
+++ P1Lab1_SimmonsKimberly.py
@@ -0,0 +1,10 @@
+#Kimberly Simmons
+#02/24/25
+#P1LAB1
+#This assignment promps the user to enter their first name then last name. Then will display a welcome message using the information entered.
+
+
+firstName = input("Enter Your First Name: ")
+lastName = input("Enter Your Last Name: ")
+
+print("Hello,", firstName, lastName, "! Welcome to CTI-110.")
